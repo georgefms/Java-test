f901b685b400e0c14c1f512a01c2e93763e5b811
georgefms <49169097+georgefms@users.noreply.github.com>
2019-09-04 14:35:32 -0300

    Update questao6.java

--- questao6.java
+++ questao6.java
@@ -2,39 +2,34 @@
 opções, sendo: 1-Soma; 2-Subtração, 3-Divisão, 4-Multiplicação; 5-Inserir novos
 números; 6-SAIR. O programa deverá apresentar o resultado da operação escolhida
 pelo usuário e em seguida voltar ao menu para uma próxima operação. */
-// Até então com problemas na função change que deveria trocar os valores recebidos na main.
+
 import java.util.Scanner;
 public class questao6
 {
-/*public static void input(int a, int b){
-    Scanner key = new Scanner(System.in);
-    System.out.println("Primeiro valor: ");
-    int num1 = key.nextInt();  
-    System.out.println("Segundo valor: ");
-    int num2 = key.nextInt();
-}*/
 public static int soma(int a,int b){
     return a+b;
 }
+
 public static int subtrai(int a, int b){
     return a-b;
 }
+
 public static int multp(int a, int b){
     return a*b;
 }
+
 public static double div(int a, int b){
     double result = a/b;
     return result;  
 }
-public static int changea(int a){
-    a = changea(a);
-    return a; //Ainda não funciona
+
+public static int change(int a){
+    Scanner key = new Scanner(System.in);
+    System.out.println("Insira o valor a ser trocado: ");
+    a = key.nextInt();
+    return a; 
 }
-public static int changeb(int b){
-    b = changeb(b);
-    return b; //tbm não 
-}
-    
+ 
 public static boolean main(String args[]){
     Scanner key = new Scanner(System.in);    
     System.out.println("Primeiro valor: ");
@@ -59,10 +54,12 @@ public static boolean main(String args[]){
         case "/":
         System.out.println(div(num1,num2));
         break;
-        /*case "trocar":
-        num1=changea(num1);
-        num2=changeb(num2);
-        break;*/
+        case "trocar":
+        System.out.println("Primeiro valor: ");
+        num1=change(num1);
+        System.out.println("Segundo valor: ");
+        num2=change(num2);
+        break;
         case "sair":
         return false;
         default:
